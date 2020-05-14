ecc99d56f804cc284f46563069d0122c840b8452
Nikhil Reddy Vatti <35322884+sainikhil1605@users.noreply.github.com>
2020-05-14 21:58:11 +0530

    Add files via upload

--- Personal_Assistant/assis.py
+++ Personal_Assistant/assis.py
@@ -3,6 +3,7 @@ import speech_recognition as sr
 import datetime
 import wikipedia 
 import webbrowser
+import getpass
 import os
 import smtplib
 
@@ -27,8 +28,9 @@ def wishMe():
 
     else:
         speak("Good Evening!")  
-
-    speak("I am your personal assistant, How may I help you!!")       
+    s=(getpass.getuser())
+    s=s.lower()
+    speak("Hi"+s+"I am your personal assistant, How may I help you!!")       
 
 def takeCommand():
     r = sr.Recognizer()
@@ -59,6 +61,10 @@ def sendEmail(to, content):
 if __name__ == "__main__":
     wishMe()
     while True:
+        try: 
+            from googlesearch import search 
+        except ImportError:  
+            print("No module named 'google' found") 
         query = takeCommand().lower()
         if 'wikipedia' in query:
             speak('Searching Wikipedia...')
@@ -67,6 +73,11 @@ if __name__ == "__main__":
             speak("According to Wikipedia")
             print(results)
             speak(results)
+        elif 'search google' in query:
+            speak('Searching Google...')
+            query = query.replace("google", "")
+            for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
+                print(j) 
 
         elif 'open youtube' in query:
             webbrowser.open("youtube.com")
@@ -75,9 +86,11 @@ if __name__ == "__main__":
             webbrowser.open("google.com")
 
         elif 'open github' in query:
-            webbrowser.open("hackerrank.com")   
-
-
+            webbrowser.open("github.com")   
+        elif 'open' in query:
+            query=query.replace("open","")
+            query=query+".com"
+            webbrowser.open(query)
         elif 'play music' in query:
             music_dir = 'Enter your music directory'
             songs = os.listdir(music_dir)
@@ -97,4 +110,13 @@ if __name__ == "__main__":
                 speak("Email has been sent!")
             except Exception as e:
                 print(e)
-                speak("Sorry...Try again")    
+                speak("Sorry...Try again") 
+        elif(query!="none"):
+            print("Here are the google results for your search")
+            speak("Here are the google results for your search")
+            for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
+                print(j)
+        else:
+            speak("Sorry Didn't get you speak again")
+
+
